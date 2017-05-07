c9664c680ce02086e26fa4ffed7e56ce9d38d234
Mad-Otter <mad_otter@protonmail.ch>
2017-05-07 18:35:35 +0200

    chargement base d'app

--- filter.py
+++ filter.py
@@ -1,4 +1,5 @@
 dico = []
+baseapp = []
 
 def charger_dictionnaire():
     file = open("dictionnaire1000en.txt", "r")
@@ -18,7 +19,17 @@ def lire_message(chemin):
                 msg[word.upper()] += 1
     return msg
 
+def charger_base_app():
+    for i in range (2500):
+        baseapp.append(lire_message('baseapp/ham/'+str(i)+'.txt'))
+    # Bug pour le spam 115, qui n'est pas en utf-8
+    #for i in range (500):
+    #   baseapp.append(lire_message('baseapp/spam/'+str(i)+'.txt'))
+
 charger_dictionnaire()
+charger_base_app()
 msg = lire_message("baseapp/ham/0.txt")
 print(dico)
 print(msg)
+print(len(baseapp))
+print(baseapp[0])
